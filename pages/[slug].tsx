fb66d2b1752fb25a05adebf07469c88fd5a4d689
Jakob Hilden <jakob.hilden@boxine.de>
2021-08-27 11:44:17 +0200

    slug page

---
+++ pages/[slug].tsx
@@ -0,0 +1,30 @@
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const SlugPage = ({ data }: any) => {
+  return <div>
+    {JSON.stringify(data)}
+  </div>
+}
+
+export default SlugPage
+
+export async function getStaticProps({ params }: any) {
+  await sleep(3000)
+  console.log(params)
+  const data = { now: new Date().toLocaleString(), params }
+
+  return {
+    props: {
+      data
+    },
+    revalidate: 10
+  }
+}
+
+export async function getStaticPaths() {
+  const paths = [{ params: { slug: "foo" } }]
+
+  return { paths, fallback: false }
+}
